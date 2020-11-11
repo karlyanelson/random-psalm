819277d344f8c31fc1c34ef02b4c5eca968609de
Karly Nelson <magnet-dev+kn@asynchrony.com>
2020-11-10 18:12:49 -0600

    hook up getting passage text

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ random psalm generator using the ESV api
 https://github.com/netlify/cli/blob/master/docs/netlify-dev.md#netlify-functions
 
 https://github.com/depadiernos/token-hider-inator/blob/master/src/lambda/getapi.js
+
+## development
+
+run `netlify dev` to have a local server and be able to access functions
+
+Once you've configured the functions directory in your `netlify.toml`, the functions will be accessible through `netlify dev` server. e.g. at `http://localhost:8888/.netlify/functions/function-name`.
--- functions/getpassage.js
+++ functions/getpassage.js
@@ -1,19 +1,58 @@
-exports.handler = async function (event, context, callback) {
-  const API_PARAMS = event.queryStringParameters;
+// exports.handler = async function (event, context, callback) {
+//   const API_PARAMS = event.queryStringParameters;
 
-  const { API_TOKEN, API_URL } = process.env;
+//   const { API_TOKEN, API_URL } = process.env;
 
-  const URL = `${API_URL}?${API_PARAMS}&key=${API_TOKEN}`;
+//   const URL = `${API_URL}?${API_PARAMS}&key=${API_TOKEN}`;
 
-  // console.log("event", event);
-  // console.log("API_TOKEN", API_TOKEN);
-  // console.log("API_URL", API_URL);
-  console.log("API_PARAMS", API_PARAMS);
+//   // console.log("event", event);
+//   // console.log("API_TOKEN", API_TOKEN);
+//   // console.log("API_URL", API_URL);
+//   console.log("API_PARAMS", API_PARAMS);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      msg: "hello world",
-    }),
-  };
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       msg: "hello world",
+//     }),
+//   };
+// };
+
+const fetch = require("node-fetch");
+
+const handler = async function (event) {
+  const API_PARAMS = event.queryStringParameters.q;
+  const { API_TOKEN } = process.env;
+
+  try {
+    const response = await fetch(
+      `https://api.esv.org/v3/passage/text/?q=${API_PARAMS}`,
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Token ${API_TOKEN}`,
+        },
+      }
+    );
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: data }),
+    };
+  } catch (error) {
+    // output to netlify function log
+    console.log(error);
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    };
+  }
 };
+
+module.exports = { handler };
